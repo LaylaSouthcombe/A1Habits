ccf0f9e8b28431bc0f6718b980581dc55baf10e9
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-06 14:56:17 +0100

    seed update

--- server/db/2_seed.sql
+++ server/db/2_seed.sql
@@ -2,8 +2,8 @@
 INSERT INTO users (username, password, email) 
 VALUES
 ('igormirowski', xxxxx, igor@gmail.com),
-('leo123',xxxxx, randomemail1@gmail.com),
-('pati123',xxxxx, randomemail2@gmail.com);
+('leo123', xxxxx, leo12@gmail.com),
+('pati123', xxxxx, pati123@gmail.com);
 
 
 -- ENTRIES
@@ -20,9 +20,9 @@ VALUES
 
 INSERT INTO tracking  ( user_id, sleep, sleep_goal, exercise exercise_goal, exercise_freq, water, water_goal, smoking, smoking_goal, money, money_goal, begin_date, end_date) 
 VALUES
-    (1, true, 8, true, 4, 4, true, 6, true, 8, true, 4, '2022-06-06','2099-01-01'),
-    (2, false, 7, true, 3, 3,  true, 6, true, 8, true, 4, '2022-06-06','2099-01-01'),
-    (3, false, 6, false, 2, 2, true, 6, false, 1, false, 4, '2022-06-06','2099-01-01');
+    (1, true, 8, true, 4, 4, true, 6, true, 8, true, 4, '2022-06-06','2022-07-06'),
+    (2, false, 7, true, 3, 3,  true, 6, true, 8, true, 4, '2022-06-06','2022-07-06'),
+    (3, false, 6, false, 2, 2, true, 6, false, 1, false, 4, '2022-06-06','2022-07-06');
     
 
 
