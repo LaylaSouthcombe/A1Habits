3b5f7e72012a8b972d8272132c296e6d51bbe22a
mabus <m.buscaroli@outlook.com>
2022-06-06 19:16:06 +0100

    commented the console logs

--- client/js/utils.js
+++ client/js/utils.js
@@ -21,23 +21,23 @@ window.onload = () => {
 
 function renderSelectedPage(e) {
   const btnClicked = e.currentTarget.parentElement.id
-  console.log(
-    '[before switch case, parent] you clicked: ',
-    e.currentTarget.parentElement.id
-  )
+  // console.log(
+  //   '[before switch case, parent] you clicked: ',
+  //   e.currentTarget.parentElement.id
+  // )
   // e.currentTarget.parentElement bypasses the 'i' to 'a' and then goes to the div with an id of 'selection-n'
   switch (btnClicked) {
     case 'selection-1':
       // console.log('btnClicked: ', btnClicked)
-      console.log('clicked the home button', pageDict[btnClicked])
+      // console.log('clicked the home button', pageDict[btnClicked])
       displayPage(pageDict[btnClicked])
       break
     case 'selection-2':
-      console.log('clicked the metrics button')
+      // console.log('clicked the metrics button')
       displayPage(pageDict[btnClicked])
       break
     case 'selection-3':
-      console.log('clicked the habits button')
+      // console.log('clicked the habits button')
       displayPage(pageDict[btnClicked])
       break
     case 'selection-4':
@@ -48,21 +48,21 @@ function renderSelectedPage(e) {
 }
 // every page apart from the homepage should start with a class of 'disabled'
 function displayPage(requestedPage) {
-  console.log('requested page ***** ', requestedPage)
+  // console.log('requested page ***** ', requestedPage)
 
   const availablePages = [homepage, metricspage, habitspage]
 
   availablePages.forEach((page) => {
     if (page.id === requestedPage.id) {
-      console.log(`page is`, page)
-      console.log(` and requestedpage is`, requestedPage)
+      // console.log(`page is`, page)
+      // console.log(` and requestedpage is`, requestedPage)
       page.classList.remove('disabled')
-      console.log(` (after) and requestedpage is`, requestedPage)
+      // console.log(` (after) and requestedpage is`, requestedPage)
     } else {
       page.classList.add('disabled')
-      console.log(` other page 1 :`, homepage)
-      console.log(` other page 2 :`, metricspage)
-      console.log(` other page 3 :`, habitspage)
+      // console.log(` other page 1 :`, homepage)
+      // console.log(` other page 2 :`, metricspage)
+      // console.log(` other page 3 :`, habitspage)
     }
   })
 }
