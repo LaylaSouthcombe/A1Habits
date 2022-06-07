fb9de32420be038957f7e01a91a0330e3fc03254
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-07 14:11:31 +0100

    controller update

--- server/api/models/entry.js
+++ server/api/models/entry.js
@@ -27,55 +27,55 @@ class Entry {
 }
 
 
-// //Find by user_ID
-
-//     static findById(id){
-//         return new Promise (async (resolve, reject) => {
-//             try {
-//                 let entryData = await db.query(`SELECT * FROM entry WHERE user_id = $1;`, [ id ]);
-//                 let entry = new Event(entryData.rows[0]);
-//                 resolve (entry);
-//             } catch (err) {
-//                 reject('Entry not found');
-//                 console.log(err);
-//             }
-//         });
-//     };
-
-
-//     //create
-//     static async create(entry){
-//         return new Promise (async (resolve, reject) => {
-//             try {
-//                 const { plant_name, nickname, frequency, count } = entry;
-//                 console.log(entry);
-//                 let createdEntry = await db.query(`INSERT INTO entry (sleep, exercise, water, smoking, money, date ) VALUES ($1, $2, $3, $4) RETURNING *;`, [sleep, exercise, water, smoking, money, date]);
-//                 let newEntry = new Entry(createdEntry.rows[0]);
-//                 console.log(newEntry);
-//                 resolve  (newEntry);
-//             } catch (err) {
-//                 console.log(err);
-//                 reject('Entry could not be created');
-//             }
-//         });
-//     };
+    //create
+    static async create(entry){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const { plant_name, nickname, frequency, count } = entry;
+                console.log(entry);
+                let createdEntry = await db.query(`INSERT INTO entry (sleep, exercise, water, smoking, money, date ) VALUES ($1, $2, $3, $4) RETURNING *;`, [sleep, exercise, water, smoking, money, date]);
+                let newEntry = new Entry(createdEntry.rows[0]);
+                console.log(newEntry);
+                resolve  (newEntry);
+            } catch (err) {
+                console.log(err);
+                reject('Entry could not be created');
+            }
+        })
+    }
 
 
+//Find by user_ID
 
-//     //Delete
+    static findById(id){
+        return new Promise (async (resolve, reject) => {
+            try {
+                let entryData = await db.query(`SELECT * FROM entry WHERE user_id = $1;`, [ id ]);
+                let entry = new Event(entryData.rows[0]);
+                resolve (entry);
+            } catch (err) {
+                reject('Entry not found');
+                console.log(err);
+            }
+        })
+    }
 
-//     static deleteEntry(id) {
-//         return new Promise(async(resolve, reject) => {
-//             try {
-//                 const result = await db.query('DELETE FROM entry WHERE id = $1 RETURNING *;', [ id ]);
-//                 resolve('Entry was deleted')
-//             } catch (err) {
-//                 reject('Entry could not be deleted')
-//                 console.log(err)
-//             }
-//         })
-//     };
 
+
+
+    //Destroy
+
+    static deleteEntry(id) {
+        return new Promise(async(resolve, reject) => {
+            try {
+                const result = await db.query('DELETE FROM entry WHERE id = $1 RETURNING *;', [ id ]);
+                resolve('Entry was deleted')
+            } catch (err) {
+                reject('Entry could not be deleted')
+                console.log(err)
+            }
+        })
+    };
 
 module.exports = Entry
 
