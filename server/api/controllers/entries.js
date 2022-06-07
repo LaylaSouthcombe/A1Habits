263414b864d34fc4b0759377c1dfd3fcf904f570
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-07 16:53:35 +0100

    add routes to controllers

--- server/api/controllers/entries.js
+++ server/api/controllers/entries.js
@@ -13,14 +13,75 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.get('/users/:user_id', async (req, res) => {
+
+//// 
+router.get('/', async (req, res) => {
     try {
-       
-        res.status(200).json(entryData)
+        const entries = await Entry.all;
+        res.status(200).json(entries)
     } catch (err) {
-        res.status(500).send({ err })
+        res.status(500).json({ err })
     }
 })
+
+//SHOW 
+
+router.get('/:id', async (req, res) => {
+    try {
+        const entries = await Entry.findById(req.params.id);
+        res.status(200).json(book)
+    } catch (err) {
+        res.status(404).json({ err })
+    }
+})
+
+//CREATE
+
+router.post('/', async (req, res) => {
+    try {
+        const createEntry = await Entry.create(req.body);
+        res.status(201).json(createEntry)
+    } catch (err) {
+        res.status(422).json({ err })
+    }
+})
+
+//UPDATE
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updateEntry = await Entry.update(req.body);
+        res.status(204).json({success: true});
+    } catch (err) {
+        res.status(404).json({ err });
+    }
+})
+
+
+//DESTROY
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const entry = await Entry.findById(req.params.id);
+        const res = entry.destroy();
+        res.status(204).end();
+    } catch (err) {
+        res.status(404).json({ err });
+    }
+})
+
+//DESTROY 2nd DRAFT 
+
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const destroyEntry = await Entry.findById(req.params.id);
+//         const res = await destroyEntry.destroy();
+//         res.status(204).end();
+//     } catch (err) {
+//         res.status(404).json({ err });
+//     }
+// })
+
 
 
 module.exports = router
