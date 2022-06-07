d30014e59a34258e8aacfa1157206b9eb2bdbae2
Amir <aha000111@icloud.com>
2022-06-07 12:50:50 +0100

    updated Logo name

--- client/tests/layout.test.js
+++ client/tests/layout.test.js
@@ -10,6 +10,8 @@ const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
 
 describe('index.html', () => {
     beforeEach(() => {
+        require ('../js/app.js')
+        require('../js/utils.js')
         document.documentElement.innerHTML = html.toString();
     })
 
@@ -20,3 +22,29 @@ describe('index.html', () => {
     })
 
 })
+
+// // Testing modal Section
+// describe('body', () => {
+//     it('modal has a title', () => {
+//       let heading = document.getElementsByClassName('modal-title')
+//       expect(heading).toBeTruthy()
+//     })
+//   })
+  
+//   // Form testing
+//   describe('body', () => {
+//     it('it has a form', () => {
+//       let heading = document.querySelector('#form')
+//       expect(heading).toBeTruthy()
+//     })
+//   })
+
+//   // has footer section 
+//         describe('html', () => {
+//             test('it has a submit button', () => {
+//                 let postButton = document.getElementsByClassName('postButton');
+//                 expect(postButton).toBeTruthy();
+//             })
+
+//         })
+
