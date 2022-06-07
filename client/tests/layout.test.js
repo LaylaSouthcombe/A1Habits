609fb177d2980d00423ef85540845a1d41c88d14
Amir <aha000111@icloud.com>
2022-06-07 13:05:20 +0100

    added title

--- client/tests/layout.test.js
+++ client/tests/layout.test.js
@@ -4,32 +4,38 @@
 
 const fs = require('fs');
 const path = require('path');
-const { hasUncaughtExceptionCaptureCallback } = require('process');
+// const { hasUncaughtExceptionCaptureCallback } = require('process');
+// const { isTypedArray } = require('util/types');
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
 
 
-describe('index.html', () => {
-    beforeEach(() => {
-        require ('../js/app.js')
-        require('../js/utils.js')
-        document.documentElement.innerHTML = html.toString();
-    })
-
-    test('it has a header title', () => {
-        let header = document.querySelector('header');
-        const title = document.querySelector('title');
-        expect(title).toBeTruthy()
-    })
-
-})
-
-// // Testing modal Section
-// describe('body', () => {
-//     it('modal has a title', () => {
-//       let heading = document.getElementsByClassName('modal-title')
-//       expect(heading).toBeTruthy()
+// describe('index.html', () => {
+//     beforeEach(() => {
+//         require ('../js/app.js')
+//         require('../js/utils.js')
+//         document.documentElement.innerHTML = html.toString();
 //     })
-//   })
+
+//     test('it has a header title', () => {
+//         let header = document.querySelector('header');
+//         const title = document.querySelector('title');
+//         expect(title).toBeTruthy()
+//     })
+
+//     it('body section to contain title', () => {
+//         const title = document.querySelector('title');
+//         expect(title.textContent),toContain('Document');
+//     })
+
+// })
+
+// Testing modal Section
+describe('body', () => {
+    it('modal has a title', () => {
+      let heading = document.getElementsByClassName('modal-title')
+      expect(heading).toBeTruthy()
+    })
+  })
   
 //   // Form testing
 //   describe('body', () => {
@@ -39,12 +45,12 @@ describe('index.html', () => {
 //     })
 //   })
 
-//   // has footer section 
-//         describe('html', () => {
-//             test('it has a submit button', () => {
-//                 let postButton = document.getElementsByClassName('postButton');
-//                 expect(postButton).toBeTruthy();
-//             })
+  // has footer section 
+        describe('html', () => {
+            test('it has a submit button', () => {
+                let postButton = document.getElementsByClassName('postButton');
+                expect(postButton).toBeTruthy();
+            })
 
-//         })
+        })
 
