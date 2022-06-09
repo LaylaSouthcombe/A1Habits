0c7fcc582b4a3b7081d9c620472726f6f717f25b
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-09 18:33:45 +0100

    try and fix errconfused 5432

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -2,14 +2,22 @@ const { Pool } = require("pg");
 
 let config;
 
-if(process.env.DATABASE_URL){
-   config = {
-    connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    } 
-}
+// if(process.env.DATABASE_URL){
+//    config = {
+//     connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     } 
+// }
+
+    config = {
+     connectionString: "postgres://qrzsbdbigleumg:32d36b8f042bbfdc5d6071c8b6b8ecbe67c4be9710af1533d6563e96c163ef93@ec2-52-73-184-24.compute-1.amazonaws.com:5432/d1vjv7p0pff32e",
+         ssl: {
+             rejectUnauthorized: false
+         }
+     } 
+
 
 
 const pool = new Pool(config);
