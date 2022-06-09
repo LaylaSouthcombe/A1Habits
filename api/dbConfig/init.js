468ece28f945b6d8760ff95c52a70eaf7418d945
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-09 19:26:07 +0100

    still try to fix error

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,6 +1,6 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-let config;
+// let config;
 
 // if(process.env.DATABASE_URL){
 //    config = {
@@ -11,15 +11,15 @@ let config;
 //     } 
 // }
 
-    config = {
-     connectionString: "postgres://qrzsbdbigleumg:32d36b8f042bbfdc5d6071c8b6b8ecbe67c4be9710af1533d6563e96c163ef93@ec2-52-73-184-24.compute-1.amazonaws.com:5432/d1vjv7p0pff32e",
-         ssl: {
-             rejectUnauthorized: false
-         }
-     } 
+const { Client } = require('pg');
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
+client.connect();
 
-const pool = new Pool(config);
-
-module.exports = pool;
+module.exports = client;
