681bd54dbca231251bf02fefb767f42a0c097fc4
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-09 12:45:43 +0100

    add tests users controllers

--- server/api/tests/unit/controllers/users.spec.js
+++ server/api/tests/unit/controllers/users.spec.js
@@ -22,22 +22,26 @@ describe('users controller', () => {
         })
     });
 
-    //show
-    describe('show', () => {
-        test('it returns a user by email and a status code of 200 ', async () => {
-            let testUser = {
-                username: 'user3',
-                password: 'password3',
-                email: 'email3'
-            }
-            jest.spyOn(User, 'findByEmail')
-                .mockResolvedValue(new User(testUser));
-            const mockReq = { params: { id: 1 } }
-            await UserController.show(mockReq, mockRes);
-            expect(mockStatus).toHaveBeenCalledWith(200);
-            expect(mockJson).toHaveBeenCalledWith(new User(testUser));
-        })
-    });
+    //show getById
+
+
+    describe("show getById", () => {
+        test("it returns a user with a 200 status code", async () => {
+          let testUser = {
+            id: 1,
+            username: "Test user",
+            password_digest: "testPassword",
+            email: "test@test.com"
+            
+          };
+          jest.spyOn(User, "getById").mockResolvedValue(new User(testUser));
+    
+          const mockReq = { params: { id: 1 } };
+          await usersController.show(mockReq, mockRes);
+          expect(mockStatus).toHaveBeenCalledWith(200);
+          expect(mockJson).toHaveBeenCalledWith(new User(testUser));
+        });
+      });
     
 
     // describe('show', () => {
