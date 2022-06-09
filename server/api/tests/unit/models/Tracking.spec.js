dbaedc16431bb019ba0d6cdb7242aeb70881de3c
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-09 08:13:20 +0100

    add test for Tracking

--- server/api/tests/unit/models/Tracking.spec.js
+++ server/api/tests/unit/models/Tracking.spec.js
@@ -24,6 +24,36 @@ describe('Tracking', () => {
         })
     });
 
+      //  test to findTrackingByUsername
+
+      describe('findTrackingByUsername', () => {
+        test('tracking to be an instance of an Array on successful db query', async () => {
+            let trackingData = {
+                sleep_track: true, sleep_goal: 8,exercise_track: true, exercise_goal: 4, exercise_freq: 'week', water_track: true, water_goal: 6, smoking_track: true, smoking_goal: 8, money_track: true, money_goal: 4, money_begin_date: '2022-06-06', money_end_date: '2022-07-06'
+            }
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [trackingData] });
+            const result = await Tracking.findTrackingByUsername(1);
+            expect(result).toBeInstanceOf(Array)
+        })
+
+    });
+
+    // test to findTrackingByUserId
+
+    describe('findTrackingByUserId', () => {
+        test('it resolves with a Tracking on successful db query', async () => {
+            let trackingData = {
+              sleep_track: true, sleep_goal: 8,exercise_track: true, exercise_goal: 4, exercise_freq: 'week', water_track: true, water_goal: 6, smoking_track: true, smoking_goal: 8, money_track: true, money_goal: 4, money_begin_date: '2022-06-06', money_end_date: '2022-07-06'
+            }
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [trackingData] });
+            const result = await Tracking.findTrackingByUserId(1);
+            expect(result).toBeInstanceOf(Tracking)
+
+        })
+    });
+
     // describe('findById', () => {
     //     test('it resolves with dog on successful db query', async () => {
     //         let dogData = { id: 1, name: 'Test Dog' }
