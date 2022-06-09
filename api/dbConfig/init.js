7ea66c716a2b40fc5077f889f3e9dab491849304
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-09 19:28:42 +0100

    revert last change

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,6 +1,6 @@
-// const { Pool } = require("pg");
+const { Pool } = require("pg");
 
-// let config;
+let config;
 
 // if(process.env.DATABASE_URL){
 //    config = {
@@ -11,15 +11,15 @@
 //     } 
 // }
 
-const { Client } = require('pg');
+    config = {
+     connectionString: "postgres://qrzsbdbigleumg:32d36b8f042bbfdc5d6071c8b6b8ecbe67c4be9710af1533d6563e96c163ef93@ec2-52-73-184-24.compute-1.amazonaws.com:5432/d1vjv7p0pff32e",
+         ssl: {
+             rejectUnauthorized: false
+         }
+     } 
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
-client.connect();
 
-module.exports = client;
+const pool = new Pool(config);
+
+module.exports = pool;
